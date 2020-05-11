c158e4cf4e6a017f40255cd96e2f7911781a3435
lihaobo_glb <18392532787@163.com>
2020-05-11 22:39:46 +0800

    剑指offer27 二叉树镜像

--- src/leetcode/offer/num27/Main.java
+++ src/leetcode/offer/num27/Main.java
@@ -13,7 +13,22 @@ public class Main {
 }
 
 class Solution {
+    //递归交换，每次交换看为一个小二叉树
     public TreeNode mirrorTree(TreeNode root) {
+        if (root == null){
+            return null;
+        }
+        //如果是叶子节点直接返回
+        if (root.left == null && root.right == null){
+            return root;
+        }
+        //root的左子树遍历后的结果
+        TreeNode l = mirrorTree(root.left);
+        //root右子树遍历后的结果
+        TreeNode r = mirrorTree(root.right);
+        //交换左右
+        root.left = r;
+        root.right = l;
         return root;
     }
 }
